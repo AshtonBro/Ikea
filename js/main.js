1693a846d1df7c71d0e615d7416c2fa3b303543a
Evgenii Solovev <solovyevmeh163@gmail.com>
2020-09-30 21:57:19 +0500

    get elements, openMenu/closeMenu

--- js/main.js
+++ js/main.js
@@ -1,10 +1,39 @@
 'use strigt';
 
+
 const btnBurger = document.querySelector('.btn-burger'),
-    catalog = document.querySelector('.catalog');
+    catalog = document.querySelector('.catalog'),
+    overlay = document.querySelector('.overlay'),
+    btnClose = document.querySelector('.btn-close'),
+    catalogList = document.querySelector('.catalog-list'),
+    catalogSub = document.querySelector('.subcatalog');
 
 const openMenu = () => {
-    catalog.classList.add('active');
+    catalog.classList.add('open');
+    overlay.classList.add('active');
+};
+
+const closeMenu = () => {
+    catalog.classList.remove('open');
+    overlay.classList.remove('active');
+};
+
+const openSubMenu = (event) => {
+    event.preventDefault();
+    const target = event.target.closest('.catalog-list__item');
+    if (target) {
+        catalogSub.classList.add('subopen');
+    }
+
 };
 
 btnBurger.addEventListener('click', openMenu);
+btnClose.addEventListener('click', closeMenu);
+overlay.addEventListener('click', closeMenu);
+catalogList.addEventListener('click', openSubMenu);
+
+document.addEventListener('keydown', (event) => {
+    if (event.code === 'Escape') {
+        closeMenu();
+    }
+});
