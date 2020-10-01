b9dc3cc300d511da57bbbc6af854ef590fb4c722
Evgenii Solovev <solovyevmeh163@gmail.com>
2020-10-01 20:16:11 +0500

    loadData + getData

--- js/index.js
+++ js/index.js
@@ -7,11 +7,9 @@ import generateHeader from './generateHeader.js';
 import generateFooter from './generateFooter.js';
 import generateCatalog from './generateCatalog.js';
 import generateSubCatalog from './generateSubCatalog.js';
-
 import {
     loadData
 } from './loadData.js';
-
 
 generateHeader();
 generateFooter();
--- js/loadData.js
+++ js/loadData.js
@@ -1,16 +1,50 @@
+import {
+    getData
+} from './getData.js';
+
+
+const wishlist = ['idd005', 'idd080', 'idd045', 'idd475', 'idd012'];
+const cartList = [{
+        id: 'idd002',
+        count: 3
+    },
+    {
+        id: 'idd022',
+        count: 1
+    },
+    {
+        id: 'idd042',
+        count: 2
+    },
+];
+
 export const loadData = () => {
-    const wishlist = ['idd005', 'idd080', 'idd045', 'idd475', 'idd012'];
-    const cardList = [{
-            id: 'idd002',
-            count: 3
-        },
-        {
-            id: 'idd022',
-            count: 1
-        },
-        {
-            id: 'idd042',
-            count: 2
-        },
-    ];
+
+    if (location.search) {
+        const search = decodeURI(location.search);
+        console.log('search: ', search);
+        const prop = search.split('=')[0].slice(1);
+        console.log('prop: ', prop);
+        const value = search.split('=')[1];
+        console.log('value: ', value);
+
+        if (prop === 's') {
+            console.log(value);
+        } else if (prop === 'wishlist') {
+            getData.wishlist(wishlist, (data) => console.log(`wishlist: ${data}`));
+        } else {
+            console.log(prop, wishlist);
+
+        }
+    }
+
+    if (location.hash) {
+        console.log('location.hash: ', location.hash.slice(1));
+
+    }
+
+    if (location.pathname.includes('cart')) {
+        console.log(cartList);
+    }
+
 };
