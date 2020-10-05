9c6f7b78c9a7c44169ee3f4e84412e44cfdc5e59
Evgenii Solovev <solovyevmeh163@gmail.com>
2020-10-05 21:57:50 +0500

    Generate Header title

--- js/generateGoods.js
+++ js/generateGoods.js
@@ -2,8 +2,33 @@ import {
     getData
 } from './getData.js';
 
+
+const wishlist = ['idd005', 'idd080', 'idd045', 'idd475', 'idd012'];
+
 const generateGoods = () => {
 
+    const mainHeader = document.querySelector('.main-header');
+    const goodsList = document.querySelector('.goods-list');
+
+    if (location.pathname.includes('goods') && location.search) {
+        const search = decodeURI(location.search);
+        const prop = search.split('=')[0].slice(1);
+        const value = search.split('=')[1];
+
+        if (prop === 'search') {
+            getData.search(value, (data) => console.log(data));
+            mainHeader.textContent = `Поиск: ${value}`;
+        } else if (prop === 'wishlist') {
+            getData.wishlist(wishlist, (data) => console.dir({
+                wishlist: data
+            }));
+            mainHeader.textContent = `Список желаний`;
+        } else if (prop === 'cat' || prop === 'subcat') {
+            getData.categoty(prop, value, (data) => console.log(data));
+            mainHeader.textContent = value;
+        }
+
+    }
 };
 
 export default generateGoods;
--- js/loadData.js
+++ js/loadData.js
@@ -4,6 +4,7 @@ import {
 
 
 const wishlist = ['idd005', 'idd080', 'idd045', 'idd475', 'idd012'];
+
 const cartList = [{
         id: 'idd002',
         count: 3
@@ -19,22 +20,6 @@ const cartList = [{
 ];
 
 export const loadData = () => {
-
-    if (location.search) {
-        const search = decodeURI(location.search);
-        const prop = search.split('=')[0].slice(1);
-        const value = search.split('=')[1];
-
-        if (prop === 'search') {
-            getData.search(value, (data) => console.log(data));
-        } else if (prop === 'wishlist') {
-            getData.wishlist(wishlist, (data) => console.dir({
-                wishlist: data
-            }));
-        } else if (prop === 'cat' || prop === 'subcat') {
-            getData.categoty(prop, value, (data) => console.log(data));
-        }
-    }
 
     if (location.hash) {
         getData.item(location.hash.slice(1), (data) => console.log(data));
