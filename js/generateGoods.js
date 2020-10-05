112ae0687d751373a3007b3a26a646ce773a7255
Evgenii Solovev <solovyevmeh163@gmail.com>
2020-10-05 22:11:40 +0500

    generate <li> cards

--- js/generateGoods.js
+++ js/generateGoods.js
@@ -9,6 +9,36 @@ const generateGoods = () => {
 
     const mainHeader = document.querySelector('.main-header');
     const goodsList = document.querySelector('.goods-list');
+    const generateCards = (data) => {
+        goodsList.textContent = '';
+
+        data.forEach(item => {
+            console.log(item);
+
+            goodsList.insertAdjacentHTML('afterbegin', `
+                <li class="${item.name}">
+                    <a class="goods-item__link" href="card.html#idd001">
+                        <article class="goods-item">
+                            <div class="goods-item__img">
+                                <img src="${item.img[0]}"
+                                    data-second-image="${item.img[1]}"
+                                    alt="${item.name}">
+                            </div>
+                            <p class="goods-item__new">Новинка</p>
+                            <h3 class="goods-item__header">${item.name}</h3>
+                            <p class="goods-item__description">${item.description}</p>
+                            <p class="goods-item__price">
+                                <span class="goods-item__price-value">${item.price}</span>
+                                <span class="goods-item__currency"> ₽</span>
+                            </p>
+                            <button class="btn btn-add-card" aria-label="Добравить в корзину"
+                                data-idd="${item.id}"></button>
+                        </article>
+                    </a>
+                </li>
+            `);
+        });
+    };
 
     if (location.pathname.includes('goods') && location.search) {
         const search = decodeURI(location.search);
@@ -16,15 +46,13 @@ const generateGoods = () => {
         const value = search.split('=')[1];
 
         if (prop === 'search') {
-            getData.search(value, (data) => console.log(data));
+            getData.search(value, generateCards);
             mainHeader.textContent = `Поиск: ${value}`;
         } else if (prop === 'wishlist') {
-            getData.wishlist(wishlist, (data) => console.dir({
-                wishlist: data
-            }));
+            getData.wishlist(wishlist, generateCards);
             mainHeader.textContent = `Список желаний`;
         } else if (prop === 'cat' || prop === 'subcat') {
-            getData.categoty(prop, value, (data) => console.log(data));
+            getData.categoty(prop, value, generateCards);
             mainHeader.textContent = value;
         }
 
