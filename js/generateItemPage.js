f7a64f5463520ee755f16cc77c2cdcddc6862933
Evgenii Solovev <solovyevmeh163@gmail.com>
2020-10-06 23:16:07 +0500

    get elements

--- js/generateItemPage.js
+++ js/generateItemPage.js
@@ -4,6 +4,33 @@ import {
 
 const generateItemPage = () => {
 
+    const renderCard = (data) => {
+
+        const {
+            category,
+            count,
+            description,
+            id,
+            img,
+            name,
+            price,
+            subcategory
+        } = data;
+
+        const goodItemNew = document.querySelector('good-item__new'),
+            goodItemHeader = document.querySelector('good-item__header'),
+            goodItemDescription = document.querySelector('good-item__description'),
+            goodItemEmpty = document.querySelector('good-item__empty'),
+            goodItemPriceValue = document.querySelector('good-item__price-value'),
+            btnGood = document.querySelector('btn-good'),
+            btnAddWishlist = document.querySelector(' btn-add-wishlist');
+
+    };
+
+    if (location.hash && location.pathname.includes('card')) {
+        getData.item(location.hash.slice(1), renderCard);
+    }
+
 };
 
 export default generateItemPage;
