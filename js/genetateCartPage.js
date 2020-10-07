65c1c2c43599c8f3a31729f2126931e53acc64d8
Evgenii Solovev <solovyevmeh163@gmail.com>
2020-10-07 22:56:11 +0500

    renderCartList

--- js/genetateCartPage.js
+++ js/genetateCartPage.js
@@ -9,17 +9,16 @@ const genetateCartPage = () => {
         const cartList = document.querySelector('.cart-list');
 
         cartList.textContent = '';
-        getData.cart(userData.cartList, (data) => {
-            data.forEach(item => {
 
-                const {
-                    name: itemName,
-                    description,
-                    id,
-                    img,
-                    price,
-                    count,
-                } = item;
+        const renderCartList = (data) => {
+            data.forEach(({
+                name: itemName,
+                description,
+                id,
+                img,
+                price,
+                count
+            }) => {
 
                 let options = '';
 
@@ -34,47 +33,50 @@ const genetateCartPage = () => {
                 }
 
                 cartList.insertAdjacentHTML('beforeend', `
-                    <li class="cart-item">
-                    <div class="product">
-                        <div class="product__image-container">
-                            <img src="${img[0]}"
-                                alt="${itemName} - ${description}">
-                        </div>
-                        <div class="product__description">
-                            <h3 class="product__name">
-                                <a href="card.html#${id}">${itemName}</a></h3>
-                            <p class="product_description-text">${description}</p>
-                        </div>
-                        <div class="product__prices">
-                            <div class="product__price-type product__price-type-regular">
-                                <div>
-                                <div class="product__total product__total-regular">${price*countUser}.-</div>
-                                ${ countUser > 1 ? `<div class="product__price-regular">${price}.-</div>` : `` }
+                        <li class="cart-item">
+                        <div class="product">
+                            <div class="product__image-container">
+                                <img src="${img[0]}"
+                                    alt="${itemName} - ${description}">
+                            </div>
+                            <div class="product__description">
+                                <h3 class="product__name">
+                                    <a href="card.html#${id}">${itemName}</a></h3>
+                                <p class="product_description-text">${description}</p>
+                            </div>
+                            <div class="product__prices">
+                                <div class="product__price-type product__price-type-regular">
+                                    <div>
+                                    <div class="product__total product__total-regular">${price*countUser}.-</div>
+                                    ${ countUser > 1 ? `<div class="product__price-regular">${price}.-</div>` : `` }
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="product__controls">
+
+                                <div class="product-controls__remove">
+                                    <button type="button" class="btn btn-remove" data-idd=${id}>
+                                        <img src="image/remove-thin-24.16c1cc7a.svg" alt="Удалить товар">
+                                    </button>
+                                </div>
+                                <div class="product-controls__quantity">
+                                    <select title="Выберите количество" aria-label="Выберите количество" data-idd=${id}>
+                                        ${options}
+                                    </select>
                                 </div>
                             </div>
                         </div>
-                        <div class="product__controls">
-
-                            <div class="product-controls__remove">
-                                <button type="button" class="btn btn-remove">
-                                    <img src="image/remove-thin-24.16c1cc7a.svg" alt="Удалить товар">
-                                </button>
-                            </div>
-                            <div class="product-controls__quantity">
-                                <select title="Выберите количество" aria-label="Выберите количество">
-                                    <option value="1">1</option>
-                                    <option value="2">2</option>
-                                    <option value="3">3</option>
-                                    <option value="4">4</option>
-                                    <option value="5">5</option>
-                                </select>
-                            </div>
-                        </div>
-                    </div>
-                </li>
+                    </li>
                 `);
             });
+        };
+
+        getData.cart(userData.cartList, renderCartList);
+
+        cartList.addEventListener('change', () => {
+
         });
+
     }
 };
 
